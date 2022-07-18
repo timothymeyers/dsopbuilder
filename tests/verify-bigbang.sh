93c59bdf37e7941c0659a8c8c7234029494d730c
Tim <timothymeyers@users.noreply.github.com>
2022-07-18 15:53:13 -0400

    asdf

--- tests/verify-bigbang.sh
+++ tests/verify-bigbang.sh
@@ -6,8 +6,14 @@ for n in {1..30}
 do
     kubectl get ks,hr -A | grep 'False\|Unknown' &> /dev/null && :
     if [[ $? -eq 1 ]]; then break; fi
-
+    
     echo "Waiting for Big Bang to reconcile ... (minute $n of 30)"
+    kubectl get ks,hr -A
+    
+    if [ $((n%5)) == "0" ]; then echo "----- Full -----"; kubectl get ks,hr -A; fi
+    
+    echo "-----"
+
     sleep 60
 done
 
