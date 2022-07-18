2d4f84fab56f25fd39cb3be375e7d80b3fc15184
Tim <timothymeyers@users.noreply.github.com>
2022-07-18 15:01:29 -0400

    Update verify-bigbang.sh

--- tests/verify-bigbang.sh
+++ tests/verify-bigbang.sh
@@ -4,17 +4,17 @@ echo "Verifying Big Bang reconcilation - Timeout at 30 minutes\n\n"
 
 for n in {1..30}
 do
-    python3 main.py bb verify | grep 'False\|Unknown' &> /dev/null && :
+    kubectl get ks,hr -A | grep 'False\|Unknown' &> /dev/null && :
     if [[ $? -eq 1 ]]; then break; fi
 
     echo "Waiting for Big Bang to reconcile ... (minute $n of 30)"
     sleep 60
 done
 
-python3 main.py bb verify | grep 'False\|Unknown' &> /dev/null && :
+kubectl get ks,hr -A | grep 'False\|Unknown' &> /dev/null && :
 if [[ $? -eq 1 ]]; then exit 1; fi
 
-python3 main.py bb verify
-#kubectl get ks,hr -A
+#python3 main.py bb verify
+kubectl get ks,hr -A
 
 exit 0
