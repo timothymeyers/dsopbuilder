c7660fa221f626fed187f5a2a5c274df9700c513
Tim <timothymeyers@users.noreply.github.com>
2022-07-20 13:09:10 -0400

    rewrite verify script to use cat

--- tests/verify-bigbang.sh
+++ tests/verify-bigbang.sh
@@ -2,41 +2,26 @@
 
 echo "Verifying Big Bang reconcilation - Timeout at 60 minutes\n\n"
 
-for n in {1..30}
+for n in {1..60}
 do
-    kubectl get ks,hr -A | grep False &> /dev/null && :
-    if [[ $? -eq 1 ]]; then break; fi
+    out=$(kubectl get gitrepositories,ks,hr -A | grep 'False\|Unknown' | cat)
+
+    if [[ "$out" = "" ]]; then break; fi
     
-    echo "Waiting for Big Bang to reconcile ... (minute $n of 30)"
-    kubectl get gitrepositories,ks,hr -A | grep False
+    echo "Waiting for Big Bang to reconcile ... (minute $n of 60)"
     
-    if [ $((n%5)) == "0" ]; then echo "----- Full -----"; kubectl get gitrepositories,ks,hr -A; fi
+    
+    if [ $((n%3)) == "0" ]; then kubectl gitrepositories,get ks,hr -A; fi
     
     echo "-----"
 
     sleep 60
 done
 
-for n in {31..60}
-do
-    kubectl get ks,hr -A | grep Unknown &> /dev/null && :
-    if [[ $? -eq 1 ]]; then break; fi
-    
-    echo "Waiting for Big Bang to reconcile ... (minute $n of 30)"
-    kubectl get gitrepositories,ks,hr -A | grep Unknown
-    
-    if [ $((n%5)) == "0" ]; then echo "----- Full -----"; kubectl get gitrepositories,ks,hr -A; fi
-    
-    echo "-----"
+kubectl gitrepositories,get ks,hr -A
 
-    sleep 60
-done
+out=$(kubectl get ks,hr -A | grep 'False\|Unknown' | cat)
 
+if [[ "$out" = "" ]]; then echo "SUCCESS"; exit 0; fi
 
-kubectl get ks,hr -A | grep Unknown &> /dev/null && :
-if [[ $? -eq 1 ]]; then exit 1; fi
-
-#python3 main.py bb verify
-kubectl get ks,hr -A
-
-exit 0
+echo "Reconciliation Timed Out"
